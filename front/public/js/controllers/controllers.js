793863aa807cf93f58361bb63302e67027db3a19
Ysunn <lkz.alexis@gmail.com>
2022-05-24 12:54:49 +0200

    Feat: Add product in index.html with script.js

---
+++ front/public/js/controllers/controllers.js
@@ -0,0 +1,11 @@
+export async function getProducts() {
+    return await fetch("http://localhost:3000/api/products").then((res) => res.json());
+}
+
+export async function getProduct(id) {
+    fetch(`http://localhost:3000/api/products/${id}`).then((res) => res.json());
+}
+
+export async function order(order) {
+    fetch(`http://localhost:3000/api/products/${order}`).then((res) => res.json());
+}
---
+++ front/public/js/script.js
@@ -0,0 +1,21 @@
+import * as ProductController from "./controllers/controllers.js";
+
+addEventListener("DOMContentLoaded", async () => {
+    const productsArray = await ProductController.getProducts();
+
+    productsArray.forEach((product) => {
+        let productsDisplay = document.querySelector("#items");
+        let productLink = document.createElement("a");
+
+        productsDisplay.appendChild(productLink);
+        productLink.setAttribute("href", `./product.html?id=${product._id}`);
+
+        productLink.innerHTML = ` 
+        <article>
+          <img src="${product.imageUrl}" alt="${product.altTxt}">
+          <h3 class="productName">${product.name}</h3>
+          <p class="productDescription">${product.description}</p>
+        </article>
+        `;
+    });
+});
