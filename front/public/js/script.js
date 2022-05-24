5b7721a58805e0fbfbbcbab33d7a7bb890742769
Ysunn <lkz.alexis@gmail.com>
2022-05-24 13:06:50 +0200

    Feat : Add products in index.html with script.js

--- front/public/js/script.js
+++ front/public/js/script.js
@@ -1,21 +1,21 @@
 import * as ProductController from "./controllers/controllers.js";
 
-addEventListener("DOMContentLoaded", async () => {
+document.addEventListener("DOMContentLoaded", async () => {
     const productsArray = await ProductController.getProducts();
+    const productsContainer = document.querySelector("#items");
 
     productsArray.forEach((product) => {
-        let productsDisplay = document.querySelector("#items");
-        let productLink = document.createElement("a");
+        const productLink = document.createElement("a");
 
-        productsDisplay.appendChild(productLink);
         productLink.setAttribute("href", `./product.html?id=${product._id}`);
-
         productLink.innerHTML = ` 
             <article>
-            <img src="${product.imageUrl}" alt="${product.altTxt}">
-            <h3 class="productName">${product.name}</h3>
-            <p class="productDescription">${product.description}</p>
+                <img src="${product.imageUrl}" alt="${product.altTxt}">
+                <h3 class="productName">${product.name}</h3>
+                <p class="productDescription">${product.description}</p>
             </article>
         `;
+
+        productsContainer.appendChild(productLink);
     });
 });
